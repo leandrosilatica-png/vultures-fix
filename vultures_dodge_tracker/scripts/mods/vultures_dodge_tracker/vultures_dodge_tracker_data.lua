fe6c42051a688cca853b6cbd3a7fd050ad51d2e5
chronic <leandrosilatica@gmail.com>
2026-02-02 02:13:38 +1000

    Create vultures_dodge_tracker_data.lua file

---
+++ vultures_dodge_tracker/scripts/mods/vultures_dodge_tracker/vultures_dodge_tracker_data.lua
@@ -0,0 +1,51 @@
+local mod = get_mod("vultures_dodge_tracker")
+
+return {
+	name = mod:localize("mod_name"),
+	description = mod:localize("mod_description"),
+	is_togglable = true,
+	options = {
+		widgets = {
+			{
+				setting_id = "enable_visual_indicator",
+				type = "checkbox",
+				default_value = true,
+			},
+			{
+				setting_id = "indicator_font_size",
+				type = "numeric",
+				default_value = 32,
+				range = {12, 72},
+			},
+			{
+				setting_id = "indicator_position_x",
+				type = "numeric",
+				default_value = 960,
+				range = {0, 3840},
+			},
+			{
+				setting_id = "indicator_position_y",
+				type = "numeric",
+				default_value = 540,
+				range = {0, 2160},
+			},
+			{
+				setting_id = "enable_shot_block",
+				type = "checkbox",
+				default_value = true,
+			},
+			{
+				setting_id = "expiry_threshold",
+				type = "numeric",
+				default_value = 0.01,
+				range = {0.001, 0.5},
+				decimals_number = 3,
+			},
+			{
+				setting_id = "smg_only",
+				type = "checkbox",
+				default_value = true,
+			},
+		}
+	}
+}
