dd2c1a5e827833438719d2fec4c9ccb4a4ad0f0f
chronic <leandrosilatica@gmail.com>
2026-02-02 02:43:12 +1000

    Create vultures_dodge_tracker_localization.lua file with mod localization

---
+++ vultures_dodge_tracker/scripts/mods/vultures_dodge_tracker/vultures_dodge_tracker_localization.lua
@@ -0,0 +1,50 @@
+return {
+	mod_name = {
+		en = "Vulture's Dodge Tracker",
+	},
+	mod_description = {
+		en = "Displays an on-screen indicator for Vulture's Dodge buff status and includes automatic shot-blocking feature.",
+	},
+	enable_visual_indicator = {
+		en = "Enable Visual Indicator",
+	},
+	enable_visual_indicator_description = {
+		en = "Show on-screen indicator for buff status",
+	},
+	indicator_font_size = {
+		en = "Indicator Font Size",
+	},
+	indicator_font_size_description = {
+		en = "Size of the buff status indicator text",
+	},
+	indicator_position_x = {
+		en = "Indicator X Position",
+	},
+	indicator_position_x_description = {
+		en = "Horizontal position of the indicator on screen",
+	},
+	indicator_position_y = {
+		en = "Indicator Y Position",
+	},
+	indicator_position_y_description = {
+		en = "Vertical position of the indicator on screen",
+	},
+	enable_shot_block = {
+		en = "Enable Automatic Shot Blocking",
+	},
+	enable_shot_block_description = {
+		en = "Automatically block one shot when buff is about to expire after a critical hit (EXPERIMENTAL)",
+	},
+	expiry_threshold = {
+		en = "Shot Block Expiry Threshold",
+	},
+	expiry_threshold_description = {
+		en = "Time remaining (in seconds) before buff expiry to trigger shot blocking",
+	},
+	smg_only = {
+		en = "SMG Weapons Only",
+	},
+	smg_only_description = {
+		en = "Only apply shot blocking to SMG-type weapons (stubber, autopistol, etc.)",
+	},
+}
